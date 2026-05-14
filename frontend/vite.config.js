dc7a1c687ed46d2cefa3ef7c4467aa08aa8eb528
Poojitha <poojithap410@gmail.com>
2026-05-14 12:25:51 +0530

    updated api url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-recruitment-platform-rioo.onrender.com',
         changeOrigin: true,
         secure: false,
       },
